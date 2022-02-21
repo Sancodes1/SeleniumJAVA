d4dc2ae629aad79d762a20183491fb1302578b25
Sancodes1 <100065380+Sancodes1@users.noreply.github.com>
2022-02-21 22:37:31 +0530

    Add files via upload

---
+++ Assigment3.java
@@ -0,0 +1,14 @@
+package Week1.day2;
+
+public class Assigment3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+  int i=-40,d;
+  if(i<0);
+  System.out.print("The Given integer is negative");
+  d=i*-1;
+  System.out.print("The number -40 is converted into" + d);
+	}
+
+}

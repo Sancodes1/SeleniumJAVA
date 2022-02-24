a62f077ca526b5b56c58d2bd78c9feaee65d0f01
Sancodes1 <100065380+Sancodes1@users.noreply.github.com>
2022-02-24 18:09:35 +0530

    Add files via upload

--- Assigment2Calculator.java
+++ Assigment2Calculator.java
@@ -2,38 +2,32 @@ package Week1.day2;
 
 public class Assigment2Calculator {
 
-	public static void main(String[] args) {
-		
-		Assigment2Calculator cal = new Assigment2Calculator();
-		cal.Add();
-		cal.sub();
-		cal.mul();
-		cal.divide();
+	public int add(int num1, int num2,int num3){
+		int result=num1+num2+num3;
+		System.out.println("Addition:" + result);
+		return result;
 	}
-    public void Add() {
-int num1=1,num2=2,num3=3,a;
-a=num1+num2+num3;
-System.out.print("The sum of three integers is"+" "+a);
+	
+public int substract(int num1, int num2){
+		int result=num1-num2;
+		System.out.println("substraction :"+ result);
+		return result;
+	}
+	
+public double multiply(double num1, double num2){
+		double result=num1*num2;
+		System.out.println("multiplication :" + result);
+		return result;
+	}
+
+public float divide(float num1, float num2){
+		float result=num1/num2;
+		System.out.println("division : "+ result);
+		return result;
+	}
+	
+
+public static void main(String[] args) {
 }
-    public void sub() {
-    	int num1=5,num2=2,s;
-    	s=num1-num2;
-    	System.out.print("The sum of three integers is"+" " +s);
-    	}
-    
-    public void mul()
-    {
-    	int num1=8,num2=8,m;
-    	m=num1*num2;
-    	System.out.print("The sum of three integers is" +" "+m);
-    	}
-    public void divide()
-    {
-    	float num1=8,num2=8,d;
-    	d=num1/num2;
-    	System.out.print("The sum of three integers"+d);
-    	}
-    
-    }
 
-
+}

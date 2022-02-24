7afd15578d32ca60cac0a7d8857c6e8d836dd9bf
Sancodes1 <100065380+Sancodes1@users.noreply.github.com>
2022-02-24 18:10:31 +0530

    Add files via upload

--- Mycalculator.java
+++ Mycalculator.java
@@ -2,14 +2,15 @@ package Week1.day2;
 
 public class Mycalculator {
 
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Assigment2Calculator cal = new Assigment2Calculator();
-		
-		cal.Add();
-		cal.sub();
-		cal.mul();
-		cal.divide();
+		Assigment2Calculator calObj = new Assigment2Calculator();
+		  calObj.add(5,2,4);
+		  calObj.substract(10,2);
+		  calObj.multiply(9,7);
+		  calObj.divide(5,2);
+		 
 	}
 
 }
